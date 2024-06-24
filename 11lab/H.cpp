00bc9c490cf3d8ecd6731ec2601e114da5359e80
dinzip <153534649+dinzip@users.noreply.github.com>
2024-06-25 00:27:50 +0500

    Update H.cpp

--- 11lab/H.cpp
+++ 11lab/H.cpp
@@ -7,10 +7,8 @@ int main(){
 	int n; string s;
 	cin>>n>>s; n%=27;
 	for(int i=0; i<s.size(); i++){
-		if(int(s[i])+n>90){
-			s[i] = char(64+((n+int(s[i]))-90));
-		}
-		else s[i] = char(int(s[i])+n);
+		if(s[i]+n>90) s[i] += n-26;
+		else s[i] += n;
 	}
 	cout<<s;
 }
